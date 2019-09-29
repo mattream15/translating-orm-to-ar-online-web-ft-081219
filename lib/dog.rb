2916b08b8a728ba629e9bbe291878111525ca946
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-28 21:16:22 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,14 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/students.sqlite"
+)
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+)
+SQL
+
+class Dog <ActiveRecord::Base
 end
